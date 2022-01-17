9c50c0235eec203aa75fc757ec4b40ed22b37c0c
VladislavGolubenko <mr.golubenich@gmail.com>
2022-01-17 14:13:23 +0300

    эндпойнты на восстановление пароля, фильтрация и пагинация на транзакциях

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -3,14 +3,16 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from django.urls import path
-from .views import UserDetail, MeView, UserView, ResetPasswordView
+from .views import UserDetail, MeView, UserView, ResetPasswordView, SendMailForgotPasswordView, ForgotPasswordView
 
 
 urlpatterns = [
     path("user/", UserView.as_view()),
     path("user/<int:pk>/", UserDetail.as_view()),
     path("me/", MeView.as_view(), name="me"),
-    path("change-password/", ResetPasswordView.as_view()),
+    path("reset-password/", ResetPasswordView.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("send-mail-forgot-password/", SendMailForgotPasswordView.as_view()),
+    path("email-reset-password/", ForgotPasswordView.as_view()),
 ]
--- apps/transaction/views.py
+++ apps/transaction/views.py
@@ -5,10 +5,17 @@ from rest_framework.response import Response
 from django_filters import rest_framework as filters
 from .models import Transaction
 from .serializers import TransactionSerializer
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.filters import OrderingFilter
+
 
 class TransactionView(ListAPIView):
+
     permission_classes = [permissions.IsAuthenticated]
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
-    filterset_fields = ('id_user',)
+    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
+    ordering_fields = 'all'
+
+    def get_queryset(self):
+        return Transaction.objects.filter(id_user=self.request.user.pk)
